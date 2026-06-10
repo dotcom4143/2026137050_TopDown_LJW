e56b4c4e56ee90f2b63619d50640c5d305f04615
dotcom4143 <eostmfhektzja4143@gmail.com>
2026-06-10 17:31:47 +0900

    스킬 데이터 수정 중 세이브, 스킬 아이콘 생성 전

---
+++ My project/Assets/Script/Dungeon/PlayerMovement/Arrow/Skills/SkillData/SkillData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SkillData", menuName = "Scriptable Objects/SkillData")]
+public class SkillData : ScriptableObject
+{
+    public float damage;
+    public float cooldown;
+    public float duration;
+}
